d899b7c31be07c245ce7fadde91790837d539e0a
Austin Tsai <paconbacon@gmail.com>
2017-09-26 17:24:22 -0400

    Create sorter.h

---
+++ sorter.h
@@ -0,0 +1,37 @@
+#ifndef _first_h
+#define _first_h
+
+typedef struct Node{
+  char *color;
+  char director_name;
+  int num_critic_for_reviews;
+  int duration;
+  int director_facebook_likes;
+  int actor_3_facebook_likes;
+  char *actor_2_name;
+  int actor_1_facebook_likes;
+  int gross;
+  char *genres;
+  char *actor_1_name;
+  char *movie_title;
+  int num_voted_users;
+  int cast_total_facebook_likes;
+  char *actor_3_name;
+  int facenumber_in_poster;
+  char *plot_keywords;
+  char *movie_imdb_link;
+  int num_users_for_reviews;
+  char *language;
+  char *country;
+  char *content_rating;
+  int budget;
+  int title_year;
+  int actor_2_facebook_likes;
+  int imbd_score;
+  int aspect_ratio;
+  int movie_facebook_likes;
+} node;
+
+
+
+#endif
